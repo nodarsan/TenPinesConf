7b84864583628a53e38f82fe702447c5eab68eb2
Matias <mmelendi@10pines.com>
2014-12-04 16:14:01 -0300

    login page tests user_factory added

---
+++ spec/features/login_page_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe 'Login page' , :js => true do
+
+  before(:each) do
+    create(:santi_user)
+    create(:mati_user)
+    create(:interesting_talk)
+  end
+
+  it 'should redirect to login page' do
+    visit '#/home'
+    click_link 'Login'
+    expect(page).to have_content('Person:')
+  end
+
+  it 'should contains the title of login page' do
+    visit '#/login'
+    expect(page).to have_content('Person:')
+  end
+
+  it 'an unregistered user shouldn´t look any talk' do
+    visit '#/talk-list'
+    expect(page).to_not have_content('Un Titulo Muy Interesante')
+  end
+
+  it 'should be ok, if santi sign in he could look your proposed talks' do
+    visit '#/login'
+    fill_in 'mail', with: 'snodar@10pines.com'
+    fill_in 'pass' , with: 'unaPassword'
+    click_button 'Submit'
+    visit '#/talk-list'
+    expect(page).to have_content('Un Titulo Muy Interesante')
+  end
+
+end
