8480c5f01bcb3d5a9f988742814e3198a7aa9209
Matias <mmelendi@10pines.com>
2014-12-09 12:06:01 -0300

    logout_spec added

---
+++ spec/features/logout_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'Logout', :js => true do
+
+  before(:each) do
+    create(:mati_user)
+    visit '#/login'
+    fill_in 'mail', with: 'mmelendi@10pines.com'
+    fill_in 'pass' , with: 'otraPassword'
+    click_button 'Submit'
+  end
+
+  it 'shouldn´t show the current user' do
+    expect(page).to have_content('User: mmelendi@10pines.com')
+    click_link 'Log out'
+    alert = page.driver.browser.switch_to.alert
+    expect(alert.text).to eq('Bye bye! :)')
+
+    expect(page).to_not have_content('User: mmelendi@10pines.com')
+  end
+
+  it 'shouldn´t show section my talks' do
+    expect(page).to have_content('MY TALKS')
+    click_link 'Log out'
+    expect(page).to_not have_content('MY TALKS')
+  end
+
+  it 'should show login and sign up links' do
+    click_link 'Log out'
+    expect(page).to have_content('Sign Up')
+    expect(page).to have_content('Login')
+  end
+
+end
