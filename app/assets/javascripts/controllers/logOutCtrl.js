7c35b570f7584a3d41e6a36895e381984c7e9a8b
Santiago Nodar <snodar@10pines.com>
2014-12-09 11:58:13 -0300

    small refactor to authentication service

--- app/assets/javascripts/controllers/logOutCtrl.js
+++ app/assets/javascripts/controllers/logOutCtrl.js
@@ -1,4 +1,5 @@
 tenPinesConfControllers.controller('LogOutCtrl', ['$scope', 'authentication',
     function($scope, authentication) {
         authentication.logout();
+        alert('Bye bye! :)');
     }]);
