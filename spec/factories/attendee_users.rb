159a78bd33e672c39faa80d98fcda12ef07a2422
Matias <mmelendi@10pines.com>
2014-12-17 13:54:42 -0300

    tested payment gateway and attendee users

--- spec/factories/attendee_users.rb
+++ spec/factories/attendee_users.rb
@@ -1,6 +1,15 @@
 FactoryGirl.define do
-  factory :attendee_user do
-    
+
+  factory :santi_attendee_user, class: AttendeeUser do
+    email 'snodar@10pines.com'
+    password 'unaPassword'
+    attendee {create(:santiago_nodar_att)}
+  end
+
+  factory :mati_attendee_user, class: AttendeeUser do
+    email 'mmelendi@10pines.com'
+    password 'otraPassword'
+    attendee {create(:matias_melendi_att)}
   end
 
 end
--- spec/models/attendee_user_spec.rb
+++ spec/models/attendee_user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe AttendeeUser, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe AttendeeUser do
+
+  before do
+    @santi = create(:santi_attendee_user)
+  end
+
+  it 'should be valid' do
+    expect(@santi.valid?).to be_truthy
+  end
+
+  it 'should be able to authorize  with correct data after being created' do
+    authenticated_user = AttendeeUser.find_by_email('snodar@10pines.com')
+    expect(authenticated_user).to eq(@santi)
+    expect(authenticated_user.valid_password?('unaPassword')).to be_truthy
+  end
+
+  it 'should not be able to authorize with incorrect mail' do
+    expect{AttendeeUser.find_by_email!('un_email_invalido@10pines.com')}.to raise_error
+  end
+
+  it 'should not be able to authorize with incorrect password' do
+    authenticated_user = AttendeeUser.find_by_email('snodar@10pines.com')
+    expect(authenticated_user).to eq(@santi)
+    expect(authenticated_user.valid_password?('123')).to be_falsey
+  end
+
 end
