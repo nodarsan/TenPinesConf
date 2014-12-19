a092cfa19fb4cdf8042a97af423497dc2370be8a
Matias <mmelendi@10pines.com>
2014-12-19 12:37:48 -0300

    -ticket cashier register ticket_sale

--- app/lib/ticket_cashier.rb
+++ app/lib/ticket_cashier.rb
@@ -15,7 +15,20 @@ class TicketCashier
         :card => token,
         :description => "Ticket: #{attendee.mail}"
     )
+    register_sale(charge, attendee)
     PurchaseMailer.purchase_mail(attendee).deliver
     charge
+  end
+
+  def register_sale(charge, attendee)
+    sale_data ={
+        stripe_id: charge[:id],
+        card_name: charge[:card][:name],
+        billing_address: charge[:card][:address_line1],
+        price: charge[:amount],
+        last4: charge[:card][:last4],
+        attendee: attendee
+    }
+    TicketSale.create!(sale_data)
   end
 end
