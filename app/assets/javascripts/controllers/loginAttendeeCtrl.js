a0874c0aae4ade7f8cc108ee364c155869f9a26f
Santiago Nodar <snodar@10pines.com>
2014-12-23 11:50:53 -0300

    refactored authentication service to use promises when logging in

--- app/assets/javascripts/controllers/loginAttendeeCtrl.js
+++ app/assets/javascripts/controllers/loginAttendeeCtrl.js
@@ -1,13 +1,14 @@
-/**
- * Created by memonono on 16/12/14.
- */
 tenPinesConfControllers.controller('loginAttendeeCtrl', ['$scope', '$location', 'authentication',
     function($scope, $location, authentication) {
         $scope.login = function() {
-            authentication.login_as_attendee($scope.email, $scope.password);
-            $location.path('/landing');
+            authentication.login_as_attendee($scope.email, $scope.password).then(
+                function(){
+                    $location.path('/landing')
+                },
+                function(error){
+                    alert(error)
+                });
         };
-
         $scope.forgotPassword = function () {
             authentication.forgot_password_attendee($scope.email);
         };
--- app/assets/javascripts/controllers/loginSpeakerCtrl.js
+++ app/assets/javascripts/controllers/loginSpeakerCtrl.js
@@ -1,8 +1,12 @@
 tenPinesConfControllers.controller('loginSpeakerCtrl', ['$scope', '$location', 'authentication',
     function($scope, $location, authentication) {
         $scope.login = function() {
-            authentication.login_as_speaker($scope.email, $scope.password);
-            $location.path('/landing');
+            authentication.login_as_speaker($scope.email, $scope.password)
+                .then(function(){
+                    $location.path('/landing')
+                }, function(reason) {
+                    alert(reason)
+                })
         };
 
         $scope.forgotPassword = function () {
